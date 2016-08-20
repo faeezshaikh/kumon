1de376d4337bee172be2974449a5fe7deba201fa
Faeez Shaikh <faeezshaikh@Faeezs-MacBook-Pro.local>
2016-08-20 18:45:23 -0500

    Theme and submitting new card

--- www/js/topicController.js
+++ www/js/topicController.js
@@ -1,13 +1,13 @@
 angular.module('starter.controllers')
 
 
-.controller('TopicCtrl',function($scope,$rootScope,$timeout,$ionicScrollDelegate,$stateParams,CardService,$window,$state,$location) {
+.controller('TopicCtrl',function($scope,$rootScope,$timeout,$ionicScrollDelegate,$stateParams,CardService,$window,$state,$location,$ionicModal) {
 	
 	console.log('Id = ',$stateParams.topicId);
 	
 	$scope.cards = CardService.getCardsForTopic($stateParams.topicId);
 	$scope.title = CardService.getTitleForTopic($stateParams.topicId);
-
+	$scope.card = {'category':'','front':'','back':''};
 	
 	$scope.manage = function() {
 		console.log('managing..',$stateParams.topicId);
@@ -18,6 +18,27 @@ angular.module('starter.controllers')
 	$scope.remove = function(card) {
 		$scope.cards.splice($scope.cards.indexOf(card), 1);
 	}
+	
+	
+	 $ionicModal.fromTemplateUrl('templates/composeFlashcard.html', {
+		    scope: $scope,
+		    animation: 'slide-in-up'
+		  }).then(function(modal) {
+		    $scope.composeIdeaModal = modal;
+		});
+	 
+	$scope.openComposer = function() {
+		$scope.composeIdeaModal.show();
+	}
+	$scope.closeComposer = function(){
+		$scope.composeIdeaModal.hide();
+	}
+	
+	$scope.submit = function(card) {
+		$scope.cards.push(card);
+		$scope.card = {};
+	}
+	
 	$scope.spin = function() {
 		  console.log('spin');
 		  $('.cardx.current').toggleClass('flip');
